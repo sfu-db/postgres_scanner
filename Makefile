52c3ae2821e66eaabec9d8be31e81d091dc4e0b6
wangxiaoying <wangxiaoying0369@gmail.com>
2023-06-13 02:39:06 +0800

    add test scripts

--- Makefile
+++ Makefile
@@ -7,6 +7,8 @@ ifeq (${OSX_BUILD_UNIVERSAL}, 1)
 	OSX_BUILD_UNIVERSAL_FLAG=-DOSX_BUILD_UNIVERSAL=1
 endif
 
+include .env
+
 # BUILD_OUT_OF_TREE_EXTENSION
 POSTGRES_SCANNER_PATH=${PWD}
 
@@ -17,6 +19,7 @@ clean:
 pull:
 	git submodule init
 	git submodule update --recursive --remote
+	cd duckdb && git checkout d58ab188ff171387ff7052bc54e78d5a17dacfeb
 
 
 debug:
@@ -26,15 +29,28 @@ debug:
 	cmake --build .
 
 
-release: pull
+# release: pull
+release:
 	mkdir -p build/release && \
 	cd build/release && \
 	cmake -DCMAKE_BUILD_TYPE=Release ${OSX_BUILD_UNIVERSAL_FLAG} -DBUILD_TPCH_EXTENSION=1 -DBUILD_TPCDS_EXTENSION=1 -DEXTENSION_STATIC_BUILD=1 ../../duckdb/CMakeLists.txt -DEXTERNAL_EXTENSION_DIRECTORIES=${POSTGRES_SCANNER_PATH} -B. -S ../../duckdb && \
-	cmake --build .
+	cmake --build . --parallel
 
 
 test: release
 	./build/release/test/unittest --test-dir . "[postgres_scanner]"
+
+bench: release
+	export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH} &&\
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH} &&\
+	$(CXX) -g -std=c++11 bench.cc -o bench -lduckdb -Lbuild/release/src -Iduckdb/src/include && \
+	./bench ${TPCH_DB} ${S3_ACCESS_KEY} ${S3_SECRETE_KEY} ${PG_CONN} ${PG_EXTENSION_PATH_RELEASE} $(QPATH) ${LOCAL_FILE_PATH}
+
+bench_tpch: release 
+	export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH} &&\
+	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH} &&\
+	$(CXX) -g -std=c++11 bench_tpch.cc -o bench_tpch -lduckdb -Lbuild/release/src -Iduckdb/src/include && \
+	./bench_tpch ${TPCH_DB} ${S3_ACCESS_KEY} ${S3_SECRETE_KEY} ${PG_CONN} ${PG_EXTENSION_PATH_RELEASE} $(QPATH) ${LOCAL_FILE_PATH} $(NRUNS)
 
 format: pull
 	cp duckdb/.clang-format .
---
+++ bench_tpch.cc
@@ -0,0 +1,156 @@
+#include "duckdb.hpp"
+#include <unistd.h>
+#include <ctime>
+#include <chrono>
+#include <fstream>
+#include <streambuf>
+#include <regex>
+#include <iostream>
+#include <iomanip>
+
+using namespace duckdb;
+
+void setupS3(Connection &con, string &s3_access_key, string &s3_secret_key, bool load_tpch = false) {
+    // Load extension httpfs
+    con.Query("INSTALL httpfs");
+    con.Query("LOAD httpfs");
+
+    con.Query("INSTALL parquet");
+    con.Query("LOAD parquet");
+
+    // Set S3 and create view
+    con.Query("SET s3_region='us-west-2'");
+    con.Query("SET s3_access_key_id='" + s3_access_key + "'");
+    con.Query("SET s3_secret_access_key='" + s3_secret_key + "'");
+
+    if (load_tpch) {
+        con.Query("create or replace view s3_lineitem as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/lineitem/*')");
+        con.Query("create or replace view s3_orders as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/orders/*')");
+        con.Query("create or replace view s3_customer as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/customer/*')");
+        con.Query("create or replace view s3_part as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/part/*')");
+        con.Query("create or replace view s3_supplier as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/supplier/*')");
+        con.Query("create or replace view s3_partsupp as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/partsupp/*')");
+        con.Query("create or replace view s3_region as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/region/*')");
+        con.Query("create or replace view s3_nation as SELECT * FROM read_parquet('s3://connector-x/redshift-unload/tpchsf10/parquet/nation/*')");
+    }
+}
+
+void setupPGScan(Connection &con, string &pg_scan_ext, string &pg_conn, bool load_tpch = false) {
+    // Load extension connectorx
+    con.Query("LOAD '" + pg_scan_ext + "'");
+
+    if (load_tpch) {
+        con.Query("create or replace view pg_lineitem as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'lineitem')");
+        con.Query("create or replace view pg_customer as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'customer')");
+        con.Query("create or replace view pg_orders as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'orders')");
+        con.Query("create or replace view pg_part as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'part')");
+        con.Query("create or replace view pg_supplier as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'supplier')");
+        con.Query("create or replace view pg_partsupp as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'partsupp')");
+        con.Query("create or replace view pg_nation as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'nation')");
+        con.Query("create or replace view pg_region as SELECT * FROM POSTGRES_SCAN('" + pg_conn + "', 'public', 'region')");
+    }
+}
+
+void setupPGScanPushdown(Connection &con, string &pg_scan_ext, string &pg_conn, bool load_tpch = false) {
+    // Load extension connectorx
+    con.Query("LOAD '" + pg_scan_ext + "'");
+
+    if (load_tpch) {
+        con.Query("create or replace view pg_lineitem as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'lineitem')");
+        con.Query("create or replace view pg_customer as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'customer')");
+        con.Query("create or replace view pg_orders as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'orders')");
+        con.Query("create or replace view pg_part as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'part')");
+        con.Query("create or replace view pg_supplier as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'supplier')");
+        con.Query("create or replace view pg_partsupp as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'partsupp')");
+        con.Query("create or replace view pg_nation as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'nation')");
+        con.Query("create or replace view pg_region as SELECT * FROM POSTGRES_SCAN_PUSHDOWN('" + pg_conn + "', 'public', 'region')");
+    }
+}
+
+void setupLocalParquet(Connection &con, string &local_path) {
+    con.Query("INSTALL parquet");
+    con.Query("LOAD parquet");
+
+    con.Query("create or replace view lc_lineitem as SELECT * FROM read_parquet('" + local_path + "/lineitem/*')");
+    con.Query("create or replace view lc_orders as SELECT * FROM read_parquet('" + local_path + "/orders/*')");
+    con.Query("create or replace view lc_customer as SELECT * FROM read_parquet('" + local_path + "/customer/*')");
+    con.Query("create or replace view lc_part as SELECT * FROM read_parquet('" + local_path + "/part/*')");
+    con.Query("create or replace view lc_supplier as SELECT * FROM read_parquet('" + local_path + "/supplier/*')");
+    con.Query("create or replace view lc_partsupp as SELECT * FROM read_parquet('" + local_path + "/partsupp/*')");
+    con.Query("create or replace view lc_region as SELECT * FROM read_parquet('" + local_path + "/region/*')");
+    con.Query("create or replace view lc_nation as SELECT * FROM read_parquet('" + local_path + "/nation/*')"); 
+}
+
+void run(Connection &con, string &query) {
+    auto start = std::chrono::system_clock::now();
+    auto res = con.Query(query);
+    // con.Query("explain " + query)->Print();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "start: " << std::ctime(&start_time) << "\nend: " << std::ctime(&end_time) << "\nduration: " << duration.count() << "s" << std::endl;
+    std::cout << "query result get " << res->RowCount() << " rows and " << res->ColumnCount() << " columns" << std::endl;
+    // res->Print();
+}
+
+/**
+* Parameters:
+*   argv[1]: db_path
+*   argv[2]: s3_access_key
+*   argv[3]: s3_secret_key
+*   argv[4]: pg_conn 
+*   argv[5]: pg_scan_ext
+*   argv[6]: query_path
+*   argv[7]: local_path
+*   argv[8]: num_runs
+**/
+int main(int argc, char *argv[]) {
+    DBConfig config;
+    config.options.allow_unsigned_extensions = true;
+    char *db_path = nullptr;
+    assert(argc >= 9);
+
+    if (argv[1][0] != '\0') {
+	   db_path = argv[1];
+    }
+    string s3_access_key = string(argv[2]);
+    string s3_secret_key = string(argv[3]);
+    string pg_conn = string(argv[4]);
+    string pg_scan_ext = string(argv[5]);
+    string query_path = string(argv[6]);
+    string local_path = string(argv[7]);
+    long int num_runs = atol(argv[8]);
+
+    DuckDB db(db_path, &config);
+	Connection con(db);
+
+    // Prepare S3
+    // setupS3(con, s3_access_key, s3_secret_key, true);
+
+    // Prepare local parquets
+    // setupLocalParquet(con, local_path);
+
+    // Prepare connectorx extension
+    // setupPGScan(con, pg_scan_ext, pg_conn, true);
+    setupPGScanPushdown(con, pg_scan_ext, pg_conn, true);
+
+    // Run all tpch queries
+    for (int r = 0; r < num_runs; ++r) {
+        std::cout << "====================== run " << r << " ======================" << std::endl;
+        for (int i = 1; i <= 22; ++i) {
+            std::stringstream ss;
+            ss << std::setw(2) << std::setfill('0') << i;
+            std::ifstream tmp_stream(query_path + "/q" + ss.str() + ".sql");
+            std::string query((std::istreambuf_iterator<char>(tmp_stream)),
+                        std::istreambuf_iterator<char>());
+            std::cout << std::endl << "query " << ss.str() << ": " << query << std::endl;
+
+            run(con, query);
+        }
+        std::cout << std::endl << std::endl;
+    }
+
+    return 0;
+}
